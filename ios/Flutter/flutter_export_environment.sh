7d37b0f3f4e3d4297cffda6d3b37f94c1e72074d
Maguinho <maguinho.aju@gmail.com>
2021-05-03 17:36:43 -0300

    Build do projeto clonado

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/maguinho/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/maguinho/Documents/Flutter_Course/project03_lista_tarefas"
-export "FLUTTER_TARGET=/Users/maguinho/Documents/Flutter_Course/project03_lista_tarefas/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/maguinho/Documents/Flutter_Course/projectt03_tasklist/TaskList"
+export "FLUTTER_TARGET=/Users/maguinho/Documents/Flutter_Course/projectt03_tasklist/TaskList/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
@@ -11,4 +11,4 @@ export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/maguinho/Documents/Flutter_Course/project03_lista_tarefas/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/maguinho/Documents/Flutter_Course/projectt03_tasklist/TaskList/.dart_tool/package_config.json"
